3b64ae1c4c99cf1e631ff794c55761b0fe06b350
Lukáš Drahník <ldrahnik@gmail.com>
2023-12-22 15:40:14 +0100

    Removed usage of odiapi as it returns product url which uses javascript

--- script.sh
+++ script.sh
@@ -12,22 +12,9 @@ BIOS_VERSION=$(echo $BIOS | cut -d '.' -f 2)
 echo "Detected laptop: $BIOS_PRODUCT_NAME"
 echo "Detected BIOS version: $BIOS_VERSION"
 
-# 1st attempt - odinapi
 USER_AGENT="user-agent-name-here"
-CURL_LAPTOP_LIST_URL="https://odinapi.asus.com/recent-data/apiv2/SearchResult?SystemCode=asus&WebsiteCode=global&SearchKey=$BIOS_PRODUCT_NAME&SearchType=products&PageSize=50&Pages=1&LocalFlag=0&siteID=www&sitelang="
-LAPTOP_LIST_CURL=$(curl -s --user-agent "$USER_AGENT" "$CURL_LAPTOP_LIST_URL" )
-
-# take only first product url (e.g. ROG Zephyrus G16 GU603ZI has multiple)
-LAPTOP_HELPDESK_BIOS=$(echo $LAPTOP_LIST_CURL | jq -r '[.Result.List[].ProductURL][0]' | tr '[:upper:]' '[:lower:]')
-LAPTOP_PD_WEB_PATH=$(echo $LAPTOP_LIST_CURL | jq -r '[.Result.List[].PDWebPath][0]' | tr '[:upper:]' '[:lower:]')
-
-# 2nd attempt - directly support page when odinapi does not return any product
-if [ -z "$LAPTOP_HELPDESK_BIOS" ]; then
-    BIOS_PRODUCT_NAME_LOWER=$(echo $BIOS_PRODUCT_NAME | tr '[:upper:]' '[:lower:]')
-    CURL_LAPTOP_SUPPORT_PAGE_URL="https://www.asus.com/supportonly/$BIOS_PRODUCT_NAME_LOWER/helpdesk_bios/"
-else 
-    CURL_LAPTOP_SUPPORT_PAGE_URL="${LAPTOP_HELPDESK_BIOS}helpdesk_bios/?model2Name=${LAPTOP_PD_WEB_PATH}"
-fi
+BIOS_PRODUCT_NAME_LOWER=$(echo $BIOS_PRODUCT_NAME | tr '[:upper:]' '[:lower:]')
+CURL_LAPTOP_SUPPORT_PAGE_URL="https://www.asus.com/supportonly/$BIOS_PRODUCT_NAME_LOWER/helpdesk_bios/"
 
 LAPTOP_SUPPORT_PAGE_CURL=$(curl -s --user-agent "$USER_AGENT" "$CURL_LAPTOP_SUPPORT_PAGE_URL" )
 
